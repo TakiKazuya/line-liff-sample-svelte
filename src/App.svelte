9d9589e5922eb90903557ddd0c278b0fdb2be8d2
滝和弥 <kazup0689@gmail.com>
2022-12-10 01:35:13 +0900

    test

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import Reservation from "./routes/reservation/New.svelte"
   import { Alert } from 'sveltestrap';
 
+  console.log('VITE_LIFF_ID:', import.meta.env.VITE_LIFF_ID)
   async function init() {
     await liff.init({
       liffId: import.meta.env.VITE_LIFF_ID
