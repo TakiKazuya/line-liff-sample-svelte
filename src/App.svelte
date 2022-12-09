9393e81c2f1f4a8a0f8bc5c6110f6b7b13961cca
滝和弥 <kazup0689@gmail.com>
2022-12-10 01:05:13 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import liff from "@line/liff";
+  import Reservation from "./routes/reservation/New.svelte"
+  import { Alert } from 'sveltestrap';
+
+  async function init() {
+    await liff.init({
+      liffId: import.meta.env.VITE_LIFF_ID
+    });
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  }
+</script>
+
+{#await init()}
+{:then _}
+  <Reservation/>
+{:catch e}
+  <Alert class="alert-danger">
+    LINE認証に失敗しました。
+    {e}
+  </Alert>
+{/await}
+
+<style>
+</style>
---
+++ src/routes/reservation/New.svelte
@@ -0,0 +1,47 @@
+<script>
+  import liff from "@line/liff";
+  import { Router, Link, Route } from "svelte-routing";
+  import {Label, Input, Button} from 'sveltestrap'
+
+  let displayName = "";
+  let accessToken = liff.getAccessToken();
+  let reserved_at = ""
+
+  async function fetchProfile() {
+    const profile = await liff.getProfile()
+    displayName = profile.displayName;
+  }
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <main>
+    <div class="mx-auto md:w-2/3 w-full mt-2">
+      <Route path="/">
+        <h1>予約フォーム</h1>
+        {#await fetchProfile()}
+        {:then profile}
+          <div class='mb-2 mx-2'>
+            <p>名前：{displayName}</p>
+          </div>
+          <div class='mb-2 mx-2'>
+            <Label for='reservation-date-input' class='block mb-2'>予約日時</Label>
+            <Input id='reservation-date-input' type="datetime-local" bind:value="{reserved_at}"/>
+          </div>
+          <div class='mb-2 mx-2'>
+            <Link to="complete">
+              <Button block>送信</Button>
+            </Link>
+          </div>
+        {/await}
+        <input type="hidden" value="{accessToken}">
+      </Route>
+      <Route path="complete">
+        <h1>予約完了</h1>
+        <p>名前：{displayName}</p>
+        <p>日時：{reserved_at}</p>
+      </Route>
+    </div>
+  </main>
+</Router>
