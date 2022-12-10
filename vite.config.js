da6a2b4e81932ab2fe570b32af8c4c1644da7d68
滝和弥 <kazup0689@gmail.com>
2022-12-10 09:08:24 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === "production" ? "line-liff-sample-svelte" : "./",
+  base: process.env.NODE_ENV === "production" && process.env.GITHUB_PAGES ? "/line-liff-sample-svelte/" : "./",
 });
